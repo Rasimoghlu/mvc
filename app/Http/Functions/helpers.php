46b94fa4f9a59c1b1911d21b3d324872693382af
Rasimoghlu <nasibovsarxan92@gmail.com>
2022-08-16 02:21:32 +0400

    added symfony var-dumper

--- app/Http/Functions/helpers.php
+++ app/Http/Functions/helpers.php
@@ -70,9 +70,7 @@ if (!function_exists('dd')) {
      */
     function dd($data)
     {
-        echo '<pre>';
-        print_r($data);
-        die();
+        dump($data);
     }
 }
 
--- route/web.php
+++ route/web.php
@@ -3,10 +3,7 @@
 use Core\Facades\Router;
 
 Router::get('test', function () {
-//    $req = \Core\Facades\Request::all();
-//    dd($req);
-//    $req = new \App\Http\Controllers\UserController();
-//    dd($req->index());
+
 });
 
 Router::get('/', function () {
